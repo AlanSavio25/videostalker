69b32b3d5816be5f3bf3080834cef0640bbf7fe4
AlanSavio25 <alan.s.paul09@gmail.com>
2019-08-24 22:40:56 +0100

    change to post request

--- src/app.py
+++ src/app.py
@@ -6,15 +6,6 @@ from time import sleep
 
 app = Flask(__name__)
 
-@app.route("/face-api")
-def face():
-  vals = main.connectToFaceAPI('b8ea8ee7334149cebd7ed530acdf84d7')
-  faces = main.getFaceResponse('https://faceapi-alan.cognitiveservices.azure.com/face/v1.0/detect', 'C:/Users/alans/Desktop/29664935_1826046541029897_4816426771371512794_o.jpg', vals[0], vals[1])
-  jsonData = main.frameAverage(faces)
-  # pprint(jsonData)
-  return str(jsonData)
-
-
 @app.route("/average")
 def average():
   print("Video frames ready")
@@ -27,7 +18,8 @@ def average():
   count = 0
   for file in filenames:
     faces = main.getFaceResponse('https://faceapi-alan.cognitiveservices.azure.com/face/v1.0/detect', file, vals[0], vals[1])
-    data.append(faces)
+    if (len(faces)>0):
+      data.append(faces)
     print("%f percentage done \n" % (100*count/len(filenames)))
     time.sleep(3)
     count+=1
@@ -38,14 +30,13 @@ def average():
   finalJson = main.overallAverage(totalData)
   return str(finalJson)
 
-@app.route("/videoframe")
+@app.route("/videoframe", methods = ['GET', 'POST'])
 def videoframe():
-  videoframes.frameCapture("https://www.youtube.com/watch?v=cT1Kzk7akjQ")
-  return redirect('/average')
-
-@app.route("/frameSplit")
-def frameSplit():
-  videoframes.frameCapture("https://www.youtube.com/watch?v=NrJEFrth27Q")
+  if request.method == 'GET':
+    videoframes.frameCapture("https://www.youtube.com/watch?v=wjIes1eGAw4")
+    return redirect('/average')
+  if request.method == 'POST'
+    print("Your POST request is: " + request)
 
 if __name__ == "__main__":
     app.run(debug=True)
